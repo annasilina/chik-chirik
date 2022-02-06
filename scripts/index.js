9ff13d160ecb1510bbc788e87bc873818f020d15
Anna Silina <kalemn@yandex.ru>
2022-02-07 02:40:26 +0300

    trying to make slider

--- scripts/index.js
+++ scripts/index.js
@@ -13,3 +13,42 @@ const anchorLinks = document.querySelectorAll('a[href^="#"]');
 		 });
 	 });
  });
+
+ //слайдер для галереи
+const gallery = document.querySelector('.gallery')
+const gallerySliderList = gallery.querySelector('.gallery__slider-list');
+const gallerySliderTrack = gallery.querySelector('.gallery__slider-track');
+const gallerySlides = gallery.querySelectorAll('.gallery__slide');
+const gallerySliderButtons = gallery.querySelectorAll('.gallery__slider-dot');
+
+const gallerySliderButtonPrev = gallerySliderButtons[0];
+const gallerySliderButtonNext = gallerySliderButtons[1];
+
+let slideWidth = gallerySlides[0].offsetWidth;
+let slideIndex = 0;
+let positionStart = 0;
+let positionX1 = 0;
+let positionX2 = 0;
+let positionFinal = 0;
+let positionCheck = slideWidth / 3;
+
+ function getEvent() {
+	 if (event.type.search('touch') !== -1) {
+		 return event.touches[0]
+	 } else return event;
+ }
+
+ function swipeStart() {
+	 let event = getEvent();
+
+	 positionStart = positionX1 = event.clientX;
+
+	 document.addEventListener('touchmove', swipeAction);
+	 document.addEventListener('touchend', swipeEnd);
+	 document.addEventListener('mousemove', swipeAction);
+	 document.addEventListener('mouseup', swipeEnd);
+ }
+
+ function swipeAction() {
+	 let event = getEvent();
+ }
